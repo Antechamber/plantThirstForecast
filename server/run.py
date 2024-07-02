0f2be290e40656d3217d38b2970e4fb88b199df1
Brian Day <antechamber@Brians-MacBook-Pro.local>
2024-07-01 21:21:31 -0700

    add forecast and historical endpoints

--- server/run.py
+++ server/run.py
@@ -1,13 +1,13 @@
 from flask import Flask
 import json
 import requests
+from datetime import datetime, timedelta
 
 app = Flask(__name__, static_url_path='')
 
 # configuration
 apiKey = "aa681fd562b24ade8df211830242906"
 zipCode = "98229"
-currentWeatherURL = f"https://api.weatherapi.com/v1/current.json?q={zipCode}&key={apiKey}"
 
 
 @app.route('/')
@@ -17,8 +17,25 @@ def home():
 
 @app.route("/current")
 def current():
-    res = requests.get(currentWeatherURL)
-    return json.loads(res.text)
+    response = requests.get(
+        f"https://api.weatherapi.com/v1/current.json?q={zipCode}&key={apiKey}")
+    return json.loads(response.text)
+
+
+@app.route("/forecast")
+def current():
+    response = requests.get(
+        f"https://api.weatherapi.com/v1/forecast.json?q={zipCode}&days=3&key={apiKey}")
+    return json.loads(response.text)
+
+
+@app.route("/historical")
+def current():
+    todaysDate = datetime.now().strftime("%Y-%m-%d")
+    threeDaysAgo = (datetime.now() - timedelta(days=3)).strftime("%Y-%m-%d")
+    response = requests.get(
+        f"https://api.weatherapi.com/v1/history.json?q={zipCode}&dt={todaysDate}&end_dt={threeDaysAgo}&key={apiKey}")
+    return json.loads(response.text)
 
 
 if __name__ == '__main__':
